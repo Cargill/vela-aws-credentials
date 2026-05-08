8b96d144922220398877b245b8e2074605e57786
Jordan Brockopp <jordan_brockopp@cargill.com>
2026-05-08 10:30:02 -0500

    fix: more typos

--- pkg/plugin/variables.go
+++ pkg/plugin/variables.go
@@ -40,7 +40,7 @@ var (
 			EnvVars:  []string{"PARAMETER_SCRIPT_FORMAT", "AWS_CREDENTIALS_SCRIPT_FORMAT"},
 			FilePath: "/vela/parameters/aws-credentials/script_format,/vela/secrets/aws-credentials/script_format",
 			Name:     FlagScriptFormat,
-			Usage:    "format of AWS credentials script (ScriptFormatShell or ScriptFormatCredentialFile)",
+			Usage:    "format of AWS credentials script (shell or credential_file)",
 			Value:    ScriptFormatShell,
 		},
 		&cli.BoolFlag{
